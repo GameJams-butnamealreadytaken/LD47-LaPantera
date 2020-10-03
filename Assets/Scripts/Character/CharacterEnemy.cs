44e82ffd7c4dcbed9f3f2c0c7ed5cd64690df1c7
Johann NOVAK <johann.novak25@gmail.com>
2020-10-03 13:29:45 +0200

    Added Character scripts/prefabs

---
+++ Assets/Scripts/Character/CharacterEnemy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class CharacterEnemy : BaseCharacter
+{
+	protected new void Start()
+	{
+		base.Start();
+
+		Assert.IsNotNull(m_characterManager);
+		Assert.IsTrue(m_characterManager is EnemyManager);
+	}
+}
---
+++ Assets/Scripts/Character/CharacterPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class CharacterPlayer : BaseCharacter
+{
+	protected new void Start()
+	{
+		base.Start();
+
+		Assert.IsNotNull(m_characterManager);
+		Assert.IsTrue(m_characterManager is PlayerManager);
+	}
+}

3b58780cab1d9d0f9c8984451b37cd00a0b91c56
Johann NOVAK <johann.novak25@gmail.com>
2020-10-03 19:20:33 +0200

    Added NavMeshAgent to CharacterEnemy

--- Assets/Scripts/Character/CharacterEnemy.cs
+++ Assets/Scripts/Character/CharacterEnemy.cs
@@ -1,8 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.Assertions;
+using Mirror;
 
 public class CharacterEnemy : BaseCharacter
 {
+	NavMeshAgent m_agent;
+
+	private void Start()
+	{
+		m_agent = GetComponent<NavMeshAgent>();
+		Assert.IsNotNull(m_agent);
+	}
+
+	private void FixedUpdate()
+	{
+		//m_agent.SetDestination(new Vector3(45.0f, 0.0f, 17.0f));
+		//m_agent.isStopped = false;
+	}
+
+	private void Update()
+	{
+	}
 }

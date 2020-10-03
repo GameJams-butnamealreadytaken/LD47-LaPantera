4f126172861a7cc4496d5c95f8016aa70b3e3634
Kranck <frizzy.rastay@gmail.com>
2020-10-04 01:29:21 +0200

    (+~) Almost finalized all the inventory system

---
+++ Assets/Scripts/UI/ActionBar.cs
@@ -0,0 +1,29 @@
+﻿/**
+ *  Author : Kranck
+ *  Date : 2020/10/03
+ *  Description : Represent the action bar of the player
+ **/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionBar : MonoBehaviour
+{
+	
+	[Tooltip("The inventory slots")]
+	public GameObject[] m_slots = new GameObject[6];
+
+	public GameObject GetFreeSlot()
+	{
+		foreach (var slot in m_slots)
+		{
+			if (!slot.GetComponentInChildren<ActionBarItem>().IsOccupied())
+			{
+				return slot;
+			}
+		}
+
+		return null;	//< there is no available slot
+	}
+}

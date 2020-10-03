061a0e394cb816cf1068fc187472b92120775558
Johann NOVAK <johann.novak25@gmail.com>
2020-10-03 20:00:18 +0200

    Added AggroRadius to CharacterEnemy

--- Assets/Scripts/Character/CharacterEnemy.cs
+++ Assets/Scripts/Character/CharacterEnemy.cs
@@ -7,21 +7,37 @@ using Mirror;
 
 public class CharacterEnemy : BaseCharacter
 {
-	NavMeshAgent m_agent;
+	[Header("Enemy properties")]
+	public float m_fAggroRadius = 10.0f;
+	public bool m_bShowAggroRadius = false;
 
-	private void Start()
+	private NavMeshAgent m_agent;
+
+	private new void Start()
 	{
+		base.Start();
+
 		m_agent = GetComponent<NavMeshAgent>();
 		Assert.IsNotNull(m_agent);
 	}
 
+	void OnDrawGizmos()
+	{
+		if (m_bShowAggroRadius)
+		{
+			UnityEditor.Handles.color = Color.red;
+			UnityEditor.Handles.DrawWireDisc(transform.position, Vector3.up, m_fAggroRadius);
+		}
+	}
+
 	private void FixedUpdate()
 	{
-		//m_agent.SetDestination(new Vector3(45.0f, 0.0f, 17.0f));
-		//m_agent.isStopped = false;
+		m_agent.SetDestination(new Vector3(45.0f, 0.0f, -17.0f));
+		m_agent.isStopped = false;
 	}
 
 	private void Update()
 	{
 	}
+
 }

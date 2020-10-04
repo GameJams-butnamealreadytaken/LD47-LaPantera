957820c195dc483694efe09f9cc9c4568e3ec186
Thomas <tom.pradal@aliceadsl.fr>
2020-10-04 15:04:50 +0200

    Added random skin to players

--- Assets/Scripts/Character/CharacterPlayer.cs
+++ Assets/Scripts/Character/CharacterPlayer.cs
@@ -1,8 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Mirror;
 using UnityEngine;
 using UnityEngine.Assertions;
 
 public class CharacterPlayer : BaseCharacter
 {
+    public List<Texture> aSkins;
+    public SkinnedMeshRenderer CharacterSkin;
+
+    [SyncVar]
+    public int skinId = 0;
+
+    new public void Start()
+    {
+        base.Start();
+
+        if (isServer)
+        {
+            skinId = Random.Range(0, aSkins.Count);
+        }
+
+        CharacterSkin.material.mainTexture = aSkins[skinId];
+    }
 }
